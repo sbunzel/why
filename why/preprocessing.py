51b1c238a2ab4363cb71ae69bc8ebef52c0df1a9
Steffen Bunzel <steffen@AT-NB-093.AT.LAN>
2020-01-08 14:12:04 +0100

    Add feature names and removing correlated features

--- why/preprocessing.py
+++ why/preprocessing.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 
-__all__ = ["InsuranceTransformer"]
+__all__ = ["InsuranceTransformer", "remove_high_corrs", "get_high_corrs"]
 
 
 class InsuranceTransformer(ColumnTransformer):
@@ -51,8 +51,28 @@ class InsuranceTransformer(ColumnTransformer):
         X_t, X_v, y_t, y_v = train_test_split(X, y, stratify=y)
         X_t = self.fit_transform(X_t)
         X_v = self.transform(X_v)
+
+        cat_names, feature_names = self.get_feature_names()
+        X_t = pd.DataFrame(X_t, columns=feature_names).astype(
+            {feat: int for feat in cat_names}
+        )
+        X_v = pd.DataFrame(X_v, columns=feature_names).astype(
+            {feat: int for feat in cat_names}
+        )
+
         self.X_t, self.X_v, self.y_t, self.y_v = X_t, X_v, y_t, y_v
         return X_t, X_v, y_t, y_v
+
+    def get_feature_names(self) -> Tuple[np.ndarray]:
+        """Extract the feature names of the transformed data from the transformer
+        
+        Returns:
+            Tuple[np.ndarray]: Categorical feature names, all feature names
+        """
+        ohe = self.named_transformers_["cat"].named_steps["onehot"]
+        cat_feature_names = ohe.get_feature_names(input_features=self.config["catcols"])
+        feature_names = np.r_[cat_feature_names, self.config["numcols"]]
+        return cat_feature_names, feature_names
 
     def save_transformed(self, savepath: Path) -> Dict[str, Path]:
         """Save the Scikit-Learn transformer and the transformed data
@@ -66,15 +86,51 @@ class InsuranceTransformer(ColumnTransformer):
         assert hasattr(
             self, "X_t"
         ), "Please call 'prepare_train_valid' on your data before saving."
-        train_valid_file = (savepath / "train_valid_data.npz").resolve()
+        train_valid_folder = (savepath / "train_valid_data").resolve()
+        if not train_valid_folder.is_dir():
+            train_valid_folder.mkdir(parents=True)
         transformer_file = (savepath / "transformer.pkl").resolve()
-        np.savez(
-            train_valid_file,
-            X_train=self.X_t,
-            X_valid=self.X_v,
-            y_train=self.y_t,
-            y_valid=self.y_v,
-        )
+        if not transformer_file.parent.is_dir():
+            transformer_file.parent.mkdir(parents=True)
+        self.X_t.to_feather(train_valid_folder / "X_train.feather")
+        self.X_v.to_feather(train_valid_folder / "X_valid.feather")
+        np.savez(train_valid_folder / "targets.npz", y_train=self.y_t, y_valid=self.y_v)
         with open(transformer_file, mode="wb") as f:
             pickle.dump(self, f)
-        return {"train_valid": train_valid_file, "transformer": transformer_file}
+        return {"train_valid": train_valid_folder, "transformer": transformer_file}
+
+
+def remove_high_corrs(df: pd.DataFrame, min_corr: float = 1.0) -> pd.DataFrame:
+    """Drop highly correlated features from a DataFrame
+    
+    Args:
+        df (pd.DataFrame): DataFrame containing the features
+        min_corr (float, optional): Minimum value for Pearson correlation to consider a correlation as high. Defaults to 1.0. . Defaults to 1.0.
+    
+    Returns:
+        pd.DataFrame: The feature data with highly correlated features removed
+    """
+    high_corrs = get_high_corrs(df, min_corr)
+    return df.drop(columns=high_corrs["feature1"].unique())
+
+
+def get_high_corrs(df: pd.DataFrame, min_corr: float = 1.0) -> pd.DataFrame:
+    """Find features with high correlation
+    
+    Args:
+        df (pd.DataFrame): DataFrame containing the features
+        min_corr (float, optional): Minimum value for Pearson correlation to consider a correlation as high. Defaults to 1.0.
+    
+    Returns:
+        pd.DataFrame: The highly correlated features
+    """
+    corrs = df.corr().reset_index()
+    return (
+        pd.melt(corrs, id_vars=["index"])
+        .dropna()
+        .rename({"index": "feature1", "variable": "feature2", "value": "corr"}, axis=1)
+        .query(f"feature1 != feature2 and abs(corr) >= {min_corr}")
+        .sort_values(["corr", "feature1", "feature2"])
+        .loc[::2]
+        .reset_index(drop=True)
+    )
