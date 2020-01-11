bafdb4c5499833eaf407abcd6f9314daf9fc3fca
Steffen Bunzel <steffenbunzel@posteo.de>
2020-01-11 13:17:49 +0100

    Add prediction distribution as foundation for the inspection of local effects

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 from sklearn.inspection import plot_partial_dependence
 from stratx.partdep import plot_stratpd
@@ -12,11 +13,11 @@ import why.utils as utils
 import why.models as models
 import why.interpret as interpret
 
-st.title("Why?")
-st.header("An exploration into the world of interpretable machine learning")
+st.markdown("# Why?")
+st.markdown("**_An exploration into the world of interpretable machine learning_**")
 
 # Read the configuration and the dataset selected by the user
-st.subheader("The Dataset")
+st.markdown("## The Dataset")
 dataset = st.selectbox("Please select a dataset", ["Car Insurance Cold Calls"])
 config = utils.get_config(dataset)
 train, test = data.get_data(
@@ -30,7 +31,7 @@ trans = preprocessing.InsuranceTransformer(config)
 X_train, X_valid, y_train, y_valid = trans.prepare_train_valid(train)
 
 # Filter the dataset
-st.sidebar.subheader("Filters")
+st.sidebar.markdown("# Filters")
 to_drop = st.sidebar.multiselect(
     "Please select features to exclude",
     options=sorted(set(train.columns) - set(config["target"])),
@@ -40,14 +41,14 @@ X_train = X_train.drop(columns=to_drop, errors="ignore")
 X_valid = X_valid.drop(columns=to_drop, errors="ignore")
 
 # Specify and fit a model
-st.sidebar.header("Settings and Model Details")
-st.sidebar.subheader("Model Type")
+st.sidebar.markdown("# Settings and Model Details")
+st.sidebar.markdown("## Model Type")
 model_type = st.sidebar.selectbox("Please select a model type", ["Random Forest"])
 with st.spinner(f"Fitting the {model_type} model..."):
     m = models.fit_model(model_type, X_train, y_train)
 
 # Report model performance
-st.sidebar.subheader("Model Performance")
+st.sidebar.markdown("## Model Performance")
 thresh = st.sidebar.slider(
     "Please select a classification threshold",
     min_value=0.0,
@@ -58,18 +59,20 @@ thresh = st.sidebar.slider(
 scores, train_pred, valid_pred = models.get_model_scores(
     m, thresh, X_train, X_valid, y_train, y_valid
 )
-st.sidebar.markdown("**Metrics**")
+st.sidebar.markdown("### Metrics")
 st.sidebar.dataframe(scores)
-st.sidebar.markdown("**Validation Confusion Matrix**")
+st.sidebar.markdown("### Confusion Matrix")
 conf_matrix = models.get_confusion_matrix(y_valid, valid_pred, thresh)
 st.sidebar.dataframe(conf_matrix)
 
 # Showcase and compare interpretation methods
-st.subheader("Understanding the Model")
+st.markdown("## Understanding the Model")
 feature_names = X_train.columns
 
+st.markdown("### Global Effects")
+
 # Feature importance
-st.markdown("**Most Important Features**")
+st.markdown("#### Most Important Features")
 if st.checkbox("Calculate feature importances"):
     with st.spinner("Calculating the feature importance..."):
         imp = interpret.feature_importance(
@@ -79,7 +82,7 @@ if st.checkbox("Calculate feature importances"):
     st.pyplot()
 
 # Partial Dependence Plots
-st.markdown("**Partial Dependence of Predictions on a Feature**")
+st.markdown("#### Partial Dependence of Predictions on a Feature")
 feat = st.selectbox("Please select a feature", sorted(X_train.columns))
 if st.checkbox("Calculate model-based partial dependence"):
     plot_partial_dependence(
@@ -94,3 +97,17 @@ if st.checkbox("Calculate model-free partial dependence"):
     )
     plt.tight_layout()
     st.pyplot()
+
+st.markdown("### Local Effects")
+
+st.markdown("#### Distribution of Model Predictions")
+distribution_plot = st.empty()
+p_min, p_max = st.slider(
+    "Select range of predictions to explain",
+    min_value=valid_pred.min(),
+    max_value=valid_pred.max(),
+    value=(0.1, 0.9),
+    step=0.01,
+)
+fig, ax = models.plot_predictions(valid_pred, p_min, p_max)
+distribution_plot.pyplot()
--- why/models.py
+++ why/models.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -41,3 +44,24 @@ def get_confusion_matrix(
     return pd.DataFrame(
         metrics.confusion_matrix(y_true, y_pred > thresh), columns=columns, index=index
     )
+
+
+def plot_predictions(y_pred: np.ndarray, p_min: float, p_max: float) -> Tuple:
+    left = y_pred[y_pred <= p_min]
+    middle = y_pred[(y_pred > p_min) & (y_pred < p_max)]
+    right = y_pred[y_pred >= p_max]
+    min_max_left = np.max(left) - np.min(left)
+    min_max_middle = np.max(middle) - np.min(middle)
+    min_max_right = np.max(right) - np.min(right)
+
+    fig, ax = plt.subplots()
+    ax.hist(left, bins=int(100 * min_max_left), color="green")
+    ax.hist(middle, bins=int(100 * min_max_middle), color="grey")
+    ax.hist(right, bins=int(100 * min_max_right), color="green")
+    ax.axvline(x=p_min, color="green")
+    ax.axvline(x=p_max, color="green")
+    ax.set_title("Model Predictions on the Validation Set")
+    ax.set_xlabel("Predicted probability of class 1")
+    ax.set_ylabel("Number of predictions")
+    plt.tight_layout()
+    return fig, ax
