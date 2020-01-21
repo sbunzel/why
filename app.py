0cbf283572611632bad40d546c12420c801976af
Steffen Bunzel <steffenbunzel@posteo.de>
2020-01-21 22:37:11 +0100

    Streamline app to improve usability

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 from sklearn.inspection import plot_partial_dependence
-from stratx.partdep import plot_stratpd
 
 # from why import data, preprocessing, utils
 import why.data as data
@@ -18,44 +17,56 @@ st.markdown("# Why?")
 st.markdown("**_An exploration into the world of interpretable machine learning_**")
 
 # Read the configuration and the dataset selected by the user
-st.markdown("## The Dataset")
+st.markdown("## Inspecting the Data")
 dataset = st.selectbox("Please select a dataset", ["Car Insurance Cold Calls"])
 config = utils.get_config(dataset)
 train, test = data.get_data(
     dataset, config, (Path(__file__).parent / "data" / "raw").resolve()
 )
-st.markdown(utils.get_data_summary(train, test, config["target"]))
-st.dataframe(train, height=150)
 
-# Prepare the data for modeling
+# Summarize and show the data, potentially removing leaky features
+data_description = st.empty()
+sample_data = st.empty()
+if st.checkbox("Exclude features derived from the last contact - they might be leaky!"):
+    to_drop = config["last_contact_features"]
+    config["catcols"] = list(set(config["catcols"]) - set(to_drop))
+    config["numcols"] = list(set(config["numcols"]) - set(to_drop))
+    train = train.drop(columns=to_drop)
+    test = test.drop(columns=to_drop)
+    st.info(f"You excluded the following features: {', '.join(to_drop)}")
+
+data_description.markdown(utils.get_data_summary(train, test, config["target"]))
+sample_data.dataframe(train, height=150)
+
+# Filter the dataset
+st.sidebar.markdown("# Settings and Details")
 trans = preprocessing.InsuranceTransformer(config)
 X_train, X_valid, y_train, y_valid = trans.prepare_train_valid(train)
 
-# Filter the dataset
-st.sidebar.markdown("# Filters")
-all_feats = sorted(set(train.columns) - set(config["target"]))
-to_drop = st.sidebar.multiselect("Please select features to exclude", options=all_feats)
-to_drop = [c for c in X_train.columns if any(d in c for d in to_drop)]
-X_train = X_train.pipe(preprocessing.remove_high_corrs, min_corr=1.0).drop(
-    columns=to_drop, errors="ignore"
+min_corr = st.sidebar.slider(
+    "Mininum correlation to drop correlated features", 0.5, 1.0, 1.0, step=0.05
 )
-X_valid = X_valid.pipe(preprocessing.remove_high_corrs, min_corr=1.0).drop(
-    columns=to_drop, errors="ignore"
-)
+X_train = X_train.pipe(preprocessing.remove_high_corrs, min_corr=min_corr)
+X_valid = X_valid.pipe(preprocessing.remove_high_corrs, min_corr=min_corr)
 
 # Specify and fit a model
-st.sidebar.markdown("# Settings and Model Details")
-seed = st.sidebar.number_input("Please choose a seed", value=42)
+seed = st.sidebar.number_input(
+    "Change the seed to investigate how randomn effects might impact the model and explanations",
+    value=42,
+)
 np.random.seed(seed)
 st.sidebar.markdown("## Model Type")
-model_type = st.sidebar.selectbox("Please select a model type", ["Random Forest"])
+model_type = st.sidebar.selectbox(
+    "Change the model type to understand its impact on the explanations",
+    ["Random Forest"],
+)
 with st.spinner(f"Fitting the {model_type} model..."):
     m = models.fit_model(model_type, X_train, y_train)
 
 # Report model performance
 st.sidebar.markdown("## Model Performance")
 thresh = st.sidebar.slider(
-    "Please select a classification threshold",
+    "The classification threshold used to calculate accuracy and derive the confusion matrix",
     min_value=0.0,
     max_value=1.0,
     value=0.5,
@@ -78,8 +89,12 @@ st.markdown("### Global Effects")
 
 # Feature importance
 st.markdown("#### Most Important Features")
-if st.checkbox("Calculate feature importances"):
-    with st.spinner("Calculating the feature importance..."):
+imp_method = st.selectbox(
+    "Please select a feature importance method",
+    ["Don't plot feature importances", "Permutation Importance"],
+)
+if not imp_method == "Don't plot feature importances":
+    with st.spinner("Calculating permutation feature importances..."):
         imp = interpret.feature_importance(
             m, X_valid.values, y_valid, n_repeats=10, n_jobs=-1
         )
@@ -88,17 +103,13 @@ if st.checkbox("Calculate feature importances"):
 
 # Partial Dependence Plots
 st.markdown("#### Partial Dependence of Predictions on a Feature")
-feat = st.selectbox("Please select a feature", sorted(X_train.columns))
-if st.checkbox("Calculate model-based partial dependence"):
+feat = st.selectbox(
+    "Please select a feature",
+    ["Don't plot partial dependence"] + sorted(X_train.columns),
+)
+if not feat == "Don't plot partial dependence":
     plot_partial_dependence(
-        m, X_valid, features=[feat], feature_names=feature_names, grid_resolution=20,
-    )
-    plt.tight_layout()
-    st.pyplot()
-
-if st.checkbox("Calculate model-free partial dependence"):
-    plot_stratpd(
-        X_valid, y_valid, feat, config["target"], min_samples_leaf=20, ntrees=10,
+        m, X_valid, features=[feat], feature_names=feature_names, grid_resolution=20
     )
     plt.tight_layout()
     st.pyplot()
@@ -118,13 +129,21 @@ fig, ax = display.plot_predictions(valid_pred, p_min, p_max)
 distribution_plot.pyplot()
 
 selected_ids = np.where(np.logical_and(valid_pred >= p_min, valid_pred <= p_max))[0]
-n_feats = st.number_input(
-    "Please select the number of features to show", value=3, min_value=3, max_value=10
-)
 
-st.markdown(f"#### Shapley Values for Top {n_feats} Features")
-shap_values = interpret.shap_values(m, X_valid)
-feat_values, local_effects = interpret.shap_feature_values(
-    m, X_valid, shap_values, selected_ids, n_feats=n_feats
+local_effects_method = st.selectbox(
+    "Please select a local effects method",
+    ["Don't display local effects", "Shapley values"],
 )
-st.table(display.format_local_explanations(feat_values))
+if not local_effects_method == "Don't display local effects":
+    n_feats = st.number_input(
+        "Please select the number of features to show",
+        value=3,
+        min_value=3,
+        max_value=10,
+    )
+    st.markdown(f"#### Shapley Values for the Top {n_feats} Features")
+    shap_values = interpret.shap_values(m, X_valid)
+    feat_values, local_effects = interpret.shap_feature_values(
+        m, X_valid, shap_values, selected_ids, n_feats=n_feats
+    )
+    st.table(display.format_local_explanations(feat_values))
