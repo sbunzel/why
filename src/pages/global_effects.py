dac65ed967ce4e7a93fe85375e766f4b2184c862
Steffen Bunzel <steffenbunzel@posteo.de>
2020-03-07 00:15:16 +0100

    Add first version of global effects in multi page layout

--- src/pages/global_effects.py
+++ src/pages/global_effects.py
@@ -1,5 +1,33 @@
+from typing import Any, Dict
+
+import matplotlib.pyplot as plt
+from sklearn.inspection import plot_partial_dependence
+
 import streamlit as st
 
 
-def write():
-    st.write("Global Effects")
+def write(session: Dict[str, Any]):
+    st.title("Global Effects")
+    st.markdown("#### Partial Dependence of Predictions on a Feature")
+    feat = st.selectbox(
+        "Please select a feature",
+        ["Don't plot partial dependence"] + sorted(session["X_train"].columns),
+    )
+    if not feat == "Don't plot partial dependence":
+        dataset = st.selectbox(
+            "Please select dataset on which to calculate partial depedence",
+            ["Test", "Train"],
+        )
+        if dataset == "Train":
+            X = session["X_train"]
+        else:
+            X = session["X_valid"]
+        plot_partial_dependence(
+            session["m"],
+            X,
+            features=[feat],
+            feature_names=X.columns,
+            grid_resolution=20,
+        )
+        plt.tight_layout()
+        st.pyplot()
