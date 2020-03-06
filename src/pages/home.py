f02abd26a29179e3701c16144d6c6c5cee3b1347
Steffen Bunzel <steffenbunzel@posteo.de>
2020-03-07 00:14:50 +0100

    Factor out data preparation for example data

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,22 +1,20 @@
-from pathlib import Path
 from typing import Any, Dict
 
 import numpy as np
 import streamlit as st
 
 from why import data as data
-from why import display as display
 from why import utils as utils
+from why import display as display
 from why import models as models
 
 
 def get_state(settings: Dict[str, Any]) -> Dict[Dict[str, Any], Dict[str, Any]]:
-    config = utils.get_config(settings["dataset"])
-    X_train, X_valid, y_train, y_valid, summary = data.get_data(
-        dataset=settings["dataset"],
-        config=config,
-        datapath=(Path(__file__).parent.parent.parent / "data" / "raw").resolve(),
-    )
+
+    train, test, target = data.get_data()
+    summary = utils.get_data_summary(train, test, target=target)
+    X_train, X_valid = train.drop(columns=target), test.drop(columns=target)
+    y_train, y_valid = train[target], test[target]
 
     if settings["random_feature"]:
         X_train["RANDOM_NUM"] = np.random.rand(X_train.shape[0])
@@ -33,13 +31,13 @@ def get_state(settings: Dict[str, Any]) -> Dict[Dict[str, Any], Dict[str, Any]]:
     return {**settings, **session}
 
 
-def write(session: Dict[Dict[str, Any], Dict[str, Any]]) -> None:
+def write(session: Dict[str, Any]) -> None:
     st.markdown("# Why?")
     st.markdown("**_An exploration into the world of interpretable machine learning_**")
 
     st.markdown("## The Dataset")
     st.markdown(session["summary"])
-    st.dataframe(session["X_train"], height=300)
+    st.dataframe(session["train"], height=300)
 
     st.markdown("## Model Performance")
     fig = display.plot_precision_recall_curve(
--- src/why/data.py
+++ src/why/data.py
@@ -1,115 +1,14 @@
-from functools import partial
-from pathlib import Path
-from typing import Any, Dict, List, Tuple
-
 import pandas as pd
 import streamlit as st
 
-from .preprocessing import InsuranceTransformer
-from .utils import get_data_summary
-
-__all__ = ["get_data", "CarInsurance"]
+from .utils import get_root_dir
 
 
 @st.cache
-def get_data(dataset: str, config: Dict[str, Any], datapath: Path):
-    if dataset == "Car Insurance Cold Calls":
-        data = CarInsurance(config, datapath)
-    else:
-        raise NotImplementedError(f"No data set for {dataset} available.")
-
-    trans = InsuranceTransformer(config)
-    X_train, X_valid, y_train, y_valid = trans.prepare_train_valid(data.train)
-    summary = get_data_summary(data.train, data.test, config["target"])
-    return X_train, X_valid, y_train, y_valid, summary
-
-
-class CarInsurance:
-    def __init__(self, config: Dict[str, Any], datapath: Path) -> None:
-        """Initialize the car insurance data set
-
-        Args:
-            config (Dict[str, Any]): Configuration file for the car insurance data
-            datapath (Path): Path to the folder containing the car insurance data
-        """
-        self.config = config
-        self.datapath = datapath
-        self.train, self.test = self.prepare_data()
-
-    def prepare_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        """Read the insurance data and apply the data transformation pipeline to train and test
-
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: The prepared car insurance train and test set
-        """
-        train, test = self.get_data()
-        return train.pipe(self.preparation_pipe), test.pipe(self.preparation_pipe)
-
-    def get_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        """Read the car insurance data into a Pandas DataFrame
-
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: The raw car insurance train and test set
-        """
-        date_parser = partial(pd.to_datetime, format="%H:%M:%S")
-        train = pd.read_csv(
-            self.datapath / "carInsurance_train.csv",
-            parse_dates=["CallStart", "CallEnd"],
-            date_parser=date_parser,
-        ).set_index("Id")
-        test = pd.read_csv(
-            self.datapath / "carInsurance_test.csv",
-            parse_dates=["CallStart", "CallEnd"],
-            date_parser=date_parser,
-        ).set_index("Id")
-        return train, test
-
-    def preparation_pipe(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Combine preprocessing steps for the car insurance data in one function
-
-        Args:
-            df (pd.DataFrame): Raw car insurance data
-
-        Returns:
-            pd.DataFrame: Transformed car insurance data
-        """
-        return (
-            df.pipe(self.add_call_time_features)
-            .pipe(self.map_binary_cols)
-            .rename(self.config["colmapper"], axis=1)
-            .drop(["CallStart", "CallEnd"], axis=1)
-        )
-
-    def add_call_time_features(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Add new features based on CallStart and CallEnd
-
-        Args:
-            df (pd.DataFrame): Car insurance data
-
-        Returns:
-            pd.DataFrame: Enhanced car insurance data
-        """
-        return df.assign(
-            LastCallDurationSecs=lambda df: (
-                df["CallEnd"] - df["CallStart"]
-            ).dt.seconds,
-            LastCallHourOfDay=lambda df: df["CallStart"].dt.hour,
-        )
-
-    def map_binary_cols(
-        self, df: pd.DataFrame, cols: List[str] = ["CarLoan", "Default", "HHInsurance"]
-    ) -> pd.DataFrame:
-        """Map binary columns to more expressive values
-
-        Args:
-            df (pd.DataFrame): Car insurance data
-            cols (List[str], optional): Column names of binary columns to transform.
-                Defaults to ["CarLoan", "Default", "HHInsurance"].
-
-        Returns:
-            pd.DataFrame: Transformed car insurance data
-        """
-        df = df.copy()
-        for c in cols:
-            df[c] = df[c].map({0: "no", 1: "yes"})
-        return df
+def get_data():
+    datapath = get_root_dir() / "data" / "processed" / "carinsurance"
+    return (
+        pd.read_csv(datapath / "train.csv"),
+        pd.read_csv(datapath / "test.csv"),
+        "CarInsurance",
+    )
