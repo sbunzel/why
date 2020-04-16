43746768500ae95433a36e0f92e5b5d8a65ab1b7
Steffen Bunzel <steffenbunzel@posteo.de>
2020-04-16 23:54:51 +0200

    Simplify calculating the model predictions

--- src/why/explainer.py
+++ src/why/explainer.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted
 
-from .models import get_model_scores
-
 __all__ = ["Explainer"]
 
 
@@ -29,7 +27,8 @@ class Explainer:
             self.test = self.insert_random_num(self.test)
         self.prepare_data()
         self.maybe_fit_model()
-        self.get_model_scores()
+        self.get_predictions()
+        self.feature_names = self.train.columns
 
     def insert_random_num(self, df: pd.DataFrame) -> pd.DataFrame:
         df["RANDOM_NUM"] = np.random.rand(df.shape[0])
@@ -51,7 +50,11 @@ class Explainer:
         except NotFittedError:
             self.model = self.model.fit(self.X_train, self.y_train)
 
-    def get_model_scores(self):
-        self.train_preds, self.test_preds = get_model_scores(
-            self.model, self.X_train, self.X_test, self.y_train, self.y_test
-        )
+    def get_predictions(self) -> None:
+        if self.mode == "binary_classification":
+            self.train_preds = self.model.predict_proba(self.X_train)[:, 1]
+            self.test_preds = self.model.predict_proba(self.X_test)[:, 1]
+        else:
+            raise NotImplementedError(
+                f"Problem type {self.mode} has not been implemented yet."
+            )
