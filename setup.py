a88a48f2e99cdbd7bda777984e4fedf725dd6d77
sbunzel <steffenbunzel@posteo.de>
2020-07-02 01:06:50 +0200

    Make package installable

--- setup.py
+++ setup.py
@@ -1,8 +1,52 @@
-from setuptools import setup, find_packages
+import os
+import shutil
+import subprocess
+from distutils.cmd import Command
+from runpy import run_path
+
+from setuptools import find_packages, setup
+
+# read the program version from version.py (without loading the module)
+__version__ = run_path("src/why/version.py")["__version__"]
+
+
+def read(fname):
+    """Utility function to read the README file."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class DistCommand(Command):
+
+    description = "build the distribution packages (in the 'dist' folder)"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if os.path.exists("build"):
+            shutil.rmtree("build")
+        subprocess.run(["python", "setup.py", "sdist", "bdist_wheel"])
+
 
 setup(
     name="why",
-    version="0.1",
-    packages=find_packages(where="src"),
+    version=__version__,
+    author="Steffen Bunzel",
+    author_email="steffenbunzel@posteo.de",
+    description="An exploration of the world of interpretable machine learning",
+    license="MIT",
+    url="https://github.com/sbunzel/why",
+    packages=find_packages("src"),
     package_dir={"": "src"},
+    long_description=read("README.md"),
+    install_requires=[],
+    tests_require=["pytest", "pytest-cov", "pre-commit"],
+    cmdclass={"dist": DistCommand},
+    platforms="any",
+    python_requires=">=3.7",
+    zip_safe=False,
 )
